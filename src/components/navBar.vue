1ff14a7f45cd64c871d0a14e0b3f0bb322750677
遠天 <enten0000@gmail.com>
2022-06-14 16:24:22 +0800

    refactor:package the navigation bar

---
+++ src/components/navBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      :ellipsis="false"
+      @select="handleSelect"
+  >
+    <el-menu-item id="menu-logo">
+      <div>
+        <img src='../../public/logo.svg' style="width: 64px;" alt="Home"/>
+      </div>
+      <span id="menu-logo-text">EEStore</span>
+    </el-menu-item>
+
+    <el-menu-item index="1">机械硬盘</el-menu-item>
+    <el-menu-item index="2">固态硬盘</el-menu-item>
+    <el-menu-item index="3">固态硬盘</el-menu-item>
+    <el-menu-item index="4">存储卡</el-menu-item>
+    <el-sub-menu>
+      <template #title>按场景分类</template>
+      <el-menu-item index="5-1">家用</el-menu-item>
+      <el-menu-item index="5-2">NAS</el-menu-item>
+      <el-menu-item index="5-3">云、边缘和数据中心</el-menu-item>
+    </el-sub-menu>
+
+    <div class="flex-grow"></div>
+    <el-menu-item index="6">
+      <el-icon class="user-menu">
+        <Search/>
+      </el-icon>
+    </el-menu-item>
+    <el-menu-item index="7">
+      <el-icon class="user-menu">
+        <ShoppingCart/>
+      </el-icon>
+    </el-menu-item>
+    <el-menu-item index="8" style="margin-right: 20vh;">
+      <el-icon class="user-menu">
+        <User/>
+      </el-icon>
+    </el-menu-item>
+    
+    <div class="moveblock" ref="moveblock"></div>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "navBar"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/view/Home/index.vue
+++ src/view/Home/index.vue
@@ -1,51 +1,6 @@
 <template>
   <div>
-    <el-menu
-        :default-active="activeIndex"
-        class="el-menu-demo"
-        mode="horizontal"
-        :ellipsis="false"
-        @select="handleSelect"
-    >
-      <el-menu-item id="menu-logo">
-        <div>
-          <img src='../../../public/logo.svg' style="width: 64px;" alt="Home"/>
-        </div>
-        <span id="menu-logo-text">EEStore</span>
-      </el-menu-item>
-
-
-      <el-menu-item index="1">机械硬盘</el-menu-item>
-      <el-menu-item index="2">固态硬盘</el-menu-item>
-      <el-menu-item index="3">固态硬盘</el-menu-item>
-      <el-menu-item index="4">存储卡</el-menu-item>
-      <el-sub-menu>
-        <template #title>按场景分类</template>
-        <el-menu-item index="5-1">家用</el-menu-item>
-        <el-menu-item index="5-2">NAS</el-menu-item>
-        <el-menu-item index="5-3">云、边缘和数据中心</el-menu-item>
-      </el-sub-menu>
-
-      <div class="flex-grow"></div>
-      <el-menu-item index="6">
-        <el-icon class="user-menu">
-          <Search/>
-        </el-icon>
-      </el-menu-item>
-      <el-menu-item index="7">
-        <el-icon class="user-menu">
-          <ShoppingCart/>
-        </el-icon>
-      </el-menu-item>
-      <el-menu-item index="8" style="margin-right: 20vh;">
-        <el-icon class="user-menu">
-          <User/>
-        </el-icon>
-      </el-menu-item>
-
-
-      <div class="moveblock" ref="moveblock"></div>
-    </el-menu>
+    <nav-bar></nav-bar>
 
     <div class="block text-center">
       <el-carousel height="450px">
@@ -78,10 +33,11 @@
 <script>
 import ItemCard from "@/components/ItemCard";
 import {getItemList} from "@/api/item";
+import NavBar from "@/components/navBar";
 
 export default {
   name: 'HomePage',
-  components: {ItemCard},
+  components: {NavBar, ItemCard},
   data(){
     return{
       itemList:[
